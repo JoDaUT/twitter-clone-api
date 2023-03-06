ac4833d78532a8144766c09c07c178e70f3cc5e9
jose urena <jose.david.urena.torres@gmail.com>
2023-03-06 12:58:03 -0600

    feat(user): User resource boilerplate and config service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
+    const hello = this.configService.get<string>('HOLA');
+    console.log(hello);
     return 'Hello World!';
   }
 }
