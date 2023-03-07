067d689c7664bc8e51e44307667e4071c207c4d1
José David Ureña Torres <47344349+JoDaUT@users.noreply.github.com>
2023-03-06 22:15:08 -0600

    feat(users): Add users feature to with add, get. Follow boilerplate. TypeORM setup (#1)
    * feat(user): User resource boilerplate and config service
    
    * User module boilerplate
    
    * User resource with get and add options, typeorm setup
    
    * Async typeorm configuration
    
    * Add User to the entities module
    
    * Add boilerplate to followers feature

---
+++ src/user/user.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  // Patch,
+  Param,
+  ParseIntPipe,
+  HttpCode,
+  // Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    console.log(typeof createUserDto.age);
+    return this.userService.create(createUserDto);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
+
+  @Post('/follow/:id')
+  @HttpCode(200)
+  follow(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+    this.userService.follow(id);
+    return;
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    this.usersRepository.save(createUserDto);
+    return createUserDto;
+  }
+
+  // findAll() {
+  //   const hello = this.configService.get<string>('HOLA');
+  //   console.log(hello);
+  //   return `This action returns all user`;
+  // }
+
+  async findOne(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+    return user;
+  }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+
+  async follow(id: number) {
+    console.log(id);
+  }
+}
